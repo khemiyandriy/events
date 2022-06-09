d2462203fb12bd2c986931a51d040a3b4c56955c
Andrii Ryzhak <andrii.ryzhak@inventorsoft.co>
2022-06-09 16:18:52 +0300

    Fixes - Andrii Ryzhak

--- src/components/EventsList.vue
+++ src/components/EventsList.vue
@@ -1,54 +1,49 @@
 <template>
   <div>
     <h1>Events</h1>
+
     <ul>
-     
-      
-      <li  v-for="event in events" :key = "event.id">
+      <li
+        v-for="event in events"
+        :key="event.id"
+      >
         <router-link :to="{ name: 'eventPage', params: {id: event.id}}">
-          {{event.id}}. Event: {{event.title}}. Date: {{event.date}}. Time: {{event.time}}. 
-        </router-link> 
+          {{ event.id }}. Event: {{ event.title }}. Date: {{ event.date }}. Time: {{ event.time }}.
+        </router-link>
       </li>
-  
-
-      
-
     </ul>
+
+    <router-view :events="events" />
   </div>
 </template>
 
 <script>
-export default{
-  
+export default {
+
   data() {
     return {
       events: []
-    }},
+    };
+  },
   mounted() {
-  fetch('./db.json')
-  .then(response => response.json())
-  .then(json => {
-    setTimeout(()=>{
-    this.events = json.events;
-    }, 500)})
+    this.fetchEvents();
   },
-  props: {
-    event: {
-      type: Object
+  methods: {
+    fetchEvents() {
+      fetch('./db.json')
+        .then(response => response.json())
+        .then(json => this.events = json.events);
     }
-  },
-}
-  
-
+  }
+};
 </script>
 
 <style scoped>
-
-  li{
-    list-style: none;
-    font-size: 18px;
-    text-align: left;
-    margin: 10px;
-    }
+li {
+  list-style: none;
+  font-size: 18px;
+  text-align: left;
+  margin: 10px;
+}
 </style>
 
--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -3,11 +3,3 @@
     <h1>Error 404</h1>
   </div>
 </template>
-
-<script>
-   export default{}
-</script>
-
-<style scoped>
-
-</style>
