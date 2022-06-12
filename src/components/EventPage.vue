e8c00d6093eb3e3f11bee63492424d6b4bf9bf2b
khemiyandriy <khemiyandriy@gmail.com>
2022-06-12 16:36:28 +0300

    add form for details info about event

--- src/components/EventPage.vue
+++ src/components/EventPage.vue
@@ -1,19 +1,39 @@
 <template>
-    <div v-if="event">
+    <div v-if="event" class="form">
+      <h2>{{event.title}}</h2>
     <div
-      v-for="(value, key) in event"
-      :key="key"
-    >
-      <p v-if="key !== 'attendees'">
-        {{ key }} : {{value}}"
-      </p> 
+      v-for="(value, key, index) in event"
+      :key="index"
+    >   
+    
+      <div class="form__item" v-if="key !== 'attendees'">
 
-      <ul v-else>
-        <li
+      <label 
+      :for="key">{{key}}
+      </label>
+
+      <input 
+      type="text" 
+      id="key" 
+      v-model="event[key]">
+
+      </div> 
+
+      <ul v-else >
+        <p>Attendees</p>
+        <li class="form__item"
           v-for="(attendee, index) in value"
           :key="index"
         >
-          {{ attendee.id }}: {{ attendee.name }}
+        <label 
+        :for="key">{{attendee.id}}
+        </label>
+
+        <input 
+        type="text" 
+        id="key" 
+        v-model="attendee.name">
+
         </li>
       </ul>
         
@@ -35,5 +55,25 @@ export default{
 </script>
 
 <style scoped>
+li{
+  list-style: none;
+}
+ul{
+  padding-left: 0;
+}
+.form{
+  border: 2px solid black;
+  width: fit-content;
+  margin: 10px auto;
+  padding: 10px;
+  background: linear-gradient(rgba(9,121,39,.8) 0%, rgba(0,212,255,.8) 100%);
+}
+.form__item{
+  display: flex;
+  justify-content: space-between; 
+  margin: 5px;
+  font-size: 14px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
 
 </style>
